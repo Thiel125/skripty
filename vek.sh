7fec13d06e14a87adaafe71a8585b66fac2b020d
123 <123@456.com>
2017-02-17 10:32:11 +0100

    Patecni verze

--- vek.sh
+++ vek.sh
@@ -3,14 +3,11 @@ echo "Zadejte svůj věk:"
 read vek
 echo "Zadejte příjmení:"
 read jmeno
-if [ $jmeno == thiel ] || [ $jmeno == Thiel ]; then
-	echo "jsi děcko"
-else if [ $vek -le 14 ]; then
+if [ $vek -le 14 ]; then
 	echo "Jste dítě"
 else if [ $vek -ge 15 ] && [ $vek -le 17 ]; then
 	echo "Jste mladistvý"
 else
 	echo "Jste dospělý"
-fi
 fi
 fi
