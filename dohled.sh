bd93e341a04ae5a8aea2c7d9e423752331f17a6c
123 <123@456.com>
2017-02-21 07:18:21 +0100

    1

--- dohled.sh
+++ dohled.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 cas=`date +%T" "%D`
+
 ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
+
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
+
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
 
-počet nalogovaných uživatelů
-velikost disku, použito, použito v %
-obsazenost paměti
+uzivatele=`w | awk -F "," '{print $2}'`
+uzivatele=`echo $uzivatele | awk -F " " '{print $1}'`
 
+pamet=`free | grep Mem: | awk -F " " '{print $3}'`
+
+disk=`df -h | grep /dev/sda1 | awk -F " " '{print $2}'`
+
+pouzito=`df -h | grep /dev/sda1 | awk -F " " '{print $3}'`
+procenta=``
+#velikost disku, použito, použito v %
+
+
+
+echo "Počet uživatelů: "$uzivatele
 echo "Aktuální čas: "$cas
 echo "Moje IP: "$ip
 echo "Přijatá data: "$rx
 echo "Odeslaná data: "$tx
+echo "Obsazenost paměti: "$pamet" bytes"
+echo "Velikost disku: "$disk
+echo "Využito místa: "$pouzito
