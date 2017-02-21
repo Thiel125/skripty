bebfe354c06ddbfd7473dc85ae61cd22b02e4b57
123 456 <123@456.com>
2017-02-21 08:20:50 +0100

    Uterni verze

--- dohled.sh
+++ dohled.sh
@@ -19,14 +19,13 @@ pouzito=`df -h | grep /dev/sda1 | awk -F " " '{print $3}'`
 
 procenta=`df -h | grep /dev/sda1 | awk -F " " '{print $5}'`
 
-
-
-echo "Počet uživatelů: "$uzivatele
-echo "Aktuální čas: "$cas
-echo "Moje IP: "$ip
-echo "Přijatá data: "$rx
-echo "Odeslaná data: "$tx
-echo "Obsazenost paměti: "$pamet" bytes"
-echo "Velikost disku: "$disk
-echo "Využito místa: "$pouzito
-echo "Procentuálně: "$procenta
+echo "Počet uživatelů: "$uzivatele >> /home/student/skripty/index.html
+echo "Aktuální čas: "$cas >> /home/student/skripty/index.html
+echo "Moje IP: "$ip >> /home/student/skripty/index.html
+echo "Přijatá data: "$rx >> /home/student/skripty/index.html
+echo "Odeslaná data: "$tx >> /home/student/skripty/index.html
+echo "Obsazenost paměti: "$pamet" bytes" >> /home/student/skripty/index.html
+echo "Velikost disku: "$disk >> /home/student/skripty/index.html
+echo "Využito místa: "$pouzito >> /home/student/skripty/index.html
+echo "Procentuálně: "$procenta >> /home/student/skripty/index.html
+echo "<br><br>" >> /home/student/skripty/index.html
