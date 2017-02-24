0ba4974f5562b7d9341bccc5ee3c1d0feaade817
123 456 <123@456.com>
2017-02-24 10:43:29 +0100

    24.2.2017

--- dohled.sh
+++ dohled.sh
@@ -8,8 +8,7 @@ rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print
 
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
 
-uzivatele=`w | awk -F "," '{print $2}'`
-uzivatele=`echo $uzivatele | awk -F " " '{print $1}'`
+uzivatele=`who | wc -l`
 
 pamet=`free | grep Mem: | awk -F " " '{print $3}'`
 
@@ -19,13 +18,13 @@ pouzito=`df -h | grep /dev/sda1 | awk -F " " '{print $3}'`
 
 procenta=`df -h | grep /dev/sda1 | awk -F " " '{print $5}'`
 
-echo "Počet uživatelů: "$uzivatele >> /home/student/skripty/index.html
-echo "Aktuální čas: "$cas >> /home/student/skripty/index.html
-echo "Moje IP: "$ip >> /home/student/skripty/index.html
-echo "Přijatá data: "$rx >> /home/student/skripty/index.html
-echo "Odeslaná data: "$tx >> /home/student/skripty/index.html
-echo "Obsazenost paměti: "$pamet" bytes" >> /home/student/skripty/index.html
-echo "Velikost disku: "$disk >> /home/student/skripty/index.html
-echo "Využito místa: "$pouzito >> /home/student/skripty/index.html
-echo "Procentuálně: "$procenta >> /home/student/skripty/index.html
-echo "<br><br>" >> /home/student/skripty/index.html
+echo "Pocet uzivatelu: "$uzivatele"<br>" >> /home/student/skripty/index.html
+echo "Aktualni cas: "$cas"<br>" >> /home/student/skripty/index.html
+echo "Moje IP: "$ip"<br>" >> /home/student/skripty/index.html
+echo "Prijata data: "$rx"<br>" >> /home/student/skripty/index.html
+echo "Odeslana data: "$tx"<br>" >> /home/student/skripty/index.html
+echo "Obsazenost pameti: "$pamet"B<br>" >> /home/student/skripty/index.html
+echo "Velikost disku: "$disk"B<br>" >> /home/student/skripty/index.html
+echo "Vyuzito mista: "$pouzito"B<br>" >> /home/student/skripty/index.html
+echo "Procentualne: "$procenta"<br>" >> /home/student/skripty/index.html
+echo "<hr>" >> /home/student/skripty/index.html
